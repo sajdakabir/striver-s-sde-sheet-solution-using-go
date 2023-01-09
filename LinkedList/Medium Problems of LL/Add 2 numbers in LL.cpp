87e16bf3421db8ff6d8f0a5df934a0d8ed490e9f
sajda <sajda.kbir@gmail.com>
2023-01-09 22:20:34 +0530

    modify the add 2 numbers in LL

--- LinkedList/Medium Problems of LL/Add 2 numbers in LL.cpp
+++ LinkedList/Medium Problems of LL/Add 2 numbers in LL.cpp
@@ -1,29 +1,29 @@
+// problem link:https://leetcode.com/problems/add-two-numbers/description/
+
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int carry = 0, first, second;
-        ListNode *head = new ListNode(0), *tail = head;
-        while (l1 || l2 || carry) {
-            if (l1) {
-                first = l1->val;
-                l1 = l1->next;
+        ListNode* dummy=new ListNode(0);
+        ListNode* temp=dummy;
+        int carry=0;
+
+        while(l1!=NULL || l2!=NULL || carry){
+            int sum=0;
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
             }
-            else
-                first = 0;
-            
-            if (l2) {
-                second = l2->val;
-                l2 = l2->next;
+             if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
             }
-            else
-                second = 0;
-            
-            int temp = first+second+carry;
-            tail->next = new ListNode(temp%10);
-            tail = tail->next;
-            carry = temp/10;
-            
+            sum+=carry;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(sum%10);
+            temp->next=newNode;
+            temp=temp->next;
         }
-        return head->next;
+        return dummy->next;
     }
 };
