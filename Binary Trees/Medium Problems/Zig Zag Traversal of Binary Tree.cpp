3f5caddc5b5d5e41f9176a6ca85ab386ca4c0af3
sajda <sajda.kbir@gmail.com>
2023-01-15 16:07:48 +0530

    added another way to alobe zig zag of bt

--- Binary Trees/Medium Problems/Zig Zag Traversal of Binary Tree.cpp
+++ Binary Trees/Medium Problems/Zig Zag Traversal of Binary Tree.cpp
@@ -26,3 +26,38 @@ public:
         return ans;
     }
 };
+
+
+// another way
+
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	vector<int>ans;
+    	if(root==NULL)return ans;
+    	queue<Node*>q;
+    	q.push(root);
+    	int f=true;
+    	while(!q.empty()){
+    	    int size=q.size();
+    	    vector<int>temp(size);
+    	    
+    	    for(int i=0;i<size;i++){
+    	        Node* node=q.front();
+    	        q.pop();
+    	        int index=f?i:(size-1-i);
+    	        temp[index]=node->data;
+    	        if(node->left)q.push(node->left);
+    	        if(node->right)q.push(node->right);
+    	    }
+    	    f=!f;
+    	    for(auto it:temp){
+    	        ans.push_back(it);
+    	    }
+    	
+    	}
+    	    return ans;
+    }
+};
