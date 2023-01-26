5990ec59e5222193405e7e9d6bd7a2a0c5c566d3
sajda <sajda.kbir@gmail.com>
2023-01-27 00:09:58 +0530

    added frog jump with k distance in tabulation

--- 1D DP/Frog Jump with k distances(DP-4).cpp
+++ 1D DP/Frog Jump with k distances(DP-4).cpp
@@ -48,3 +48,33 @@ class Solution {
         return f(n-1,k,height,dp);
     }
 };
+
+
+// tabulation (bottom up)
+
+class Solution {
+  public:
+   int f(int n,int k, vector<int>& arr,vector<int>&dp){
+      dp[0]=0;
+      for(int i=1;i<n;i++){
+     int minSteps=INT_MAX;
+    for(int j=1;j<=k;j++){
+   
+        if(i-j>=0){
+           int jump=dp[i-j]+abs(arr[i]-arr[i-j]);
+             minSteps=min(jump,minSteps);
+        }
+       
+        
+    }
+    dp[i]=minSteps;
+      }
+    return dp[n-1];
+      
+  }
+    int minimizeCost(vector<int>& height, int n, int k) {
+        // Code here
+        vector<int>dp(n,-1);
+        return f(n,k,height,dp);
+    }
+};
