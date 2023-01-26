2eea38d0cd3ec35edf60965c66f0b5f6904fae9c
sajda <sajda.kbir@gmail.com>
2023-01-26 22:19:53 +0530

    frog jump memoization added

--- 1D DP/Frog Jump(DP-3).cpp
+++ 1D DP/Frog Jump(DP-3).cpp
@@ -18,6 +18,24 @@ class Solution {
 };
 // memoization (top down)
 
-
+class Solution {
+  public:
+  int f(int i, vector<int>& arr,vector<int>&dp){
+      if(i==0)return 0;
+      if(dp[i]!=-1)return dp[i];
+      int jump1=f(i-1,arr,dp)+abs(arr[i]-arr[i-1]);
+      int jump2=INT_MAX;
+      if(i>1){
+           jump2=f(i-2,arr,dp)+abs(arr[i]-arr[i-2]);
+      }
+      return dp[i]=min(jump1,jump2);
+  }
+    int minimumEnergy(vector<int>& height, int n) {
+        // Code here
+        vector<int>dp(n+1,-1);
+        
+        return f(n-1,height,dp);
+    }
+};
 // tabulation (bottom up)
 
