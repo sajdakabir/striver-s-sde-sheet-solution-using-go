b5686ba646530986cde4e94d8dc08a6a29a27980
sajda <sajda.kbir@gmail.com>
2023-01-27 11:27:48 +0530

    maximun sun of non adjcent element in memoization

--- 1D DP/Maximum sum of non-adjacent elements (DP 5).cpp
+++ 1D DP/Maximum sum of non-adjacent elements (DP 5).cpp
@@ -13,3 +13,22 @@ public:
 	    return f(n-1,arr);
 	}
 };
+
+// Memoization 
+
+
+class Solution{
+public:	
+     int f(int ind,int *arr,vector<int>&dp){
+         if(ind==0)return arr[0];
+         if(ind<0)return 0;
+         if(dp[ind]!=-1)return dp[ind];
+         int pick=arr[ind]+f(ind-2,arr,dp);
+         int notPick=0+f(ind-1,arr,dp);
+         return dp[ind]=max(pick,notPick);
+     }
+	int findMaxSum(int *arr, int n) {
+	    vector<int>dp(n,-1);
+	    return f(n-1,arr,dp);
+	}
+};
