679a2f3dfa260a2f281e21cc167f2a5ff505067c
sajda <sajda.kbir@gmail.com>
2023-01-28 14:41:56 +0530

    added house robber in memoization

--- 1D DP/House Robber.cpp
+++ 1D DP/House Robber.cpp
@@ -16,3 +16,25 @@ public:
         return f(n-1,nums);
     }
 };
+
+
+
+
+// Memoization
+
+class Solution {
+public:
+    int f(int i,vector<int>&nums,vector<int>&dp){
+        if(i==0)return nums[0];
+        if(i<0)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int rob=nums[i]+f(i-2,nums,dp);
+        int nonRob=0+f(i-1,nums,dp);
+        return dp[i]=max(rob,nonRob);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return f(n-1,nums,dp);
+    }
+};
